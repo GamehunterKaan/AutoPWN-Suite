ba83717aeeebf59fa0fe13de457ae0d364d3aa17
GamehunterKaan <kaan007.gultekin@gmail.com>
2022-05-26 20:10:57 +0300

    Update banners.py

--- modules/banners.py
+++ modules/banners.py
@@ -10,11 +10,10 @@ def print_banner():
      / ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/
     /_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/ 
 
-    """
+"""
 
-    #print it
     print_colored("\nDeveloped by GamehunterKaan.", colors.bold)
-    print_colored("\n------------------------------------------------------------------------------------------------------------------------", colors.bold)
+    print_colored("\n" + "-" * 90, colors.bold)
     print_colored(banner, colors.blue)
-    print_colored("------------------------------------------------------------------------------------------------------------------------\n", colors.bold)
+    print_colored("-" * 90 + "\n", colors.bold)
     print_colored("I am not responsible if you are doing something stupid with this tool!\n", colors.bold)
