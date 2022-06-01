86568fb9c2e6cad1c1c52f8ece5b8ca8aa27c44e
GamehunterKaan <kaan007.gultekin@gmail.com>
2022-06-01 17:31:34 +0300

    Removed class from output

--- modules/outfile.py
+++ modules/outfile.py
@@ -4,26 +4,26 @@ def InitializeOutput(context):
     global outfile
     outfile = context
 
-class output:
-    def OutputBanner(target, scantype, speed, hostfile):
-        filename = outfile
-        file = open(filename, 'w')
-        file.write("\n" + "-" * 50)
-        file.write("\n" + "AutoPWN Suite")
-        file.write("\n" + "Author: GamehunterKaan")
-        file.write("\n" + "-" * 50)
-        if hostfile:
-            file.write("\n" + "Targets list: " + hostfile)
-        else:
-            file.write("\n" + "Target: " + target)
-        file.write("\n" + "Scan type: " + scantype)
-        file.write("\n" + "Scan speed: " + str(speed))
-        file.write("\n" + "-" * 50)
-        file.write("\n" + "Started at: " + str(datetime.now()))
-        file.write("\n" + "-" * 50)
-        file.close()
-    def WriteToFile(data):
-        filename = outfile
-        file = open(filename, 'a')
-        file.write("\n" + data)
-        file.close()
+def OutputBanner(target, scantype, speed, hostfile):
+    filename = outfile
+    file = open(filename, 'w')
+    file.write("\n" + "-" * 50)
+    file.write("\n" + "AutoPWN Suite")
+    file.write("\n" + "Author: GamehunterKaan")
+    file.write("\n" + "-" * 50)
+    if hostfile:
+        file.write("\n" + "Targets list: " + hostfile)
+    else:
+        file.write("\n" + "Target: " + target)
+    file.write("\n" + "Scan type: " + scantype)
+    file.write("\n" + "Scan speed: " + str(speed))
+    file.write("\n" + "-" * 50)
+    file.write("\n" + "Started at: " + str(datetime.now()))
+    file.write("\n" + "-" * 50)
+    file.close()
+
+def WriteToFile(data):
+    filename = outfile
+    file = open(filename, 'a')
+    file.write("\n" + data)
+    file.close()
