dfff69c5d524a059ba1d82b291e7bcf9f19a4093
GamehunterKaan <kaan007.gultekin@gmail.com>
2022-06-05 20:02:22 +0300

    Updated banner

--- modules/banners.py
+++ modules/banners.py
@@ -2,18 +2,15 @@ from modules.color import print_colored, colors
 
 #https://patorjk.com/software/taag/
 def print_banner():
-    banner = """
-
-        ___           __          ____  _       __ _   __   _____         _  __      
-       /   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___ 
-      / /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\
-     / ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/
-    /_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/ 
-
-"""
+    banner = """\033[0m│        \033[94m___           __          ____  _       __ _   __   _____         _  __\033[0m         │
+│       \033[94m/   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___\033[0m    │
+│      \033[94m/ /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\\033[0m   │ 
+│     \033[94m/ ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/\033[0m   │
+│    \033[94m/_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/\033[0m    │
+│                                                                                        │"""
 
     print_colored("\nDeveloped by GamehunterKaan. (https://auto.pwnspot.com)", colors.bold)
-    print_colored("\n" + "-" * 100, colors.bold)
-    print_colored(banner.center(60), colors.blue)
-    print_colored("-" * 100 + "\n", colors.bold)
+    print_colored("\n" + "┌" + "─" * 88 + "┐", colors.bold)
+    print_colored(banner.center(90), colors.blue)
+    print_colored("└" + "─" * 88 + "┘" + "\n", colors.bold)
     print_colored("I am not responsible if you are doing something illegal using this program! \n", colors.bold)
