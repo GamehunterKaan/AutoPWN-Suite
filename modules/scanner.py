54082af0a492a519388bd69a195b0c4f45429ffa
iaacornus <iaacornus.devel@gmail.com>
2022-06-22 23:50:24 +0800

    initial refactoring

--- modules/scanner.py
+++ modules/scanner.py
@@ -1,13 +1,16 @@
-from modules.nmap import PortScanner
-from modules.logger import info, error, warning, success, println, banner, print_colored, colors, bcolors
-try:
-    from os import getuid
-except ImportError:
-    from ctypes import windll
-from multiprocessing import Process
-from dataclasses import dataclass
+from ast import Not
+from re import S
 from time import sleep
+from os import getuid
+from ctypes import windll
+from multiprocessing import Process
+
 from enum import Enum
+from dataclasses import dataclass
+
+from modules.nmap import PortScanner
+from modules.logger import Logger, banner
+from colors import bcolors
 
 @dataclass
 class PortInfo:
@@ -17,6 +20,7 @@ class PortInfo:
     service : str
     product : str
     version : str
+
 
 @dataclass()
 class TargetInfo:
@@ -29,36 +33,20 @@ class TargetInfo:
 
     def colored(self):
         return (
-            (
-                bcolors.yellow + "MAC Address : " + bcolors.endc + "{0:20}" +
-                bcolors.yellow + " Vendor : " + bcolors.endc + "{1:30}" + "\n" +
-                bcolors.yellow + "OS : " + bcolors.endc + "{2:20}" +
-                bcolors.yellow + " Accuracy : " + bcolors.endc + "{3:5}" +
-                bcolors.yellow + " Type : " + bcolors.endc + "{4:20}" + "\n"
-            ).format(
-                    str(self.mac),
-                    str(self.vendor),
-                    str(self.os[:20]),
-                    str(self.os_accuracy),
-                    str(self.os_type[:20])
-                )
+            f"{bcolors.yellow}MAC Address : {bcolors.endc} {self.mac}\n"
+            + f"{bcolors.yellow}Vendor : {bcolors.endc} {self.vendor}\n"
+            + f"{bcolors.yellow}OS : {bcolors.endc} {self.os}\n"
+            + f"{bcolors.yellow}Accuracy : {bcolors.endc} {self.os_accuracy}"
+            + f"{bcolors.yellow}Type : {bcolors.endc} {self.os_type[:20]}\n"
         )
 
     def __str__(self):
         return (
-            (
-                "MAC Address : {0:20}" +
-                " Vendor : {1:30}" + "\n" +
-                "OS : {2:20}" +
-                " Accuracy : {3:5}" +
-                " Type : {4:20}" + "\n"
-            ).format(
-                    str(self.mac),
-                    str(self.vendor),
-                    str(self.os[:20]),
-                    str(self.os_accuracy),
-                    str(self.os_type[:20])
-                )
+            f"MAC Address : {self.mac}"
+            + f" Vendor : {self.vendor}\n"
+            + f"OS : {self.os}"
+            + f" Accuracy : {self.os_accuracy}"
+            + f" Type : {self.os_type}" + "\n"
         )
 
 
@@ -67,62 +55,122 @@ class ScanMode(Enum):
     Noise = 1
     Evade = 2
 
+
 class ScanType(Enum):
     Ping = 0
     ARP = 1
 
+
 def is_root(): # this function is used everywhere, so it's better to put it here
     try:
         return getuid() == 0
-    except Exception as e:
+    except (RuntimeError, NotImplementedError):
         return windll.shell32.IsUserAnAdmin() == 1
 
+
 # this function is for turning a list of hosts into a single string
-def listToString(s): 
+def listToString(s):
     str1 = " "
     return (str1.join(s))
+
 
 #do a ping scan using nmap
 def TestPing(target, mode=ScanMode.Normal):
     nm = PortScanner()
-    if type(target) is list:
+    if isinstance(target, list):
         target = listToString(target)
     if mode == ScanMode.Evade and is_root():
-        resp = nm.scan(hosts=target, arguments="-sn -T 2 -f -g 53 --data-length 10")
+        resp = nm.scan(
+            hosts=target,
+            arguments="-sn -T 2 -f -g 53 --data-length 10"
+        )
     else:
         resp = nm.scan(hosts=target, arguments="-sn")
+
     return nm.all_hosts()
+
 
 #do a arp scan using nmap
 def TestArp(target, mode=ScanMode.Normal):
     nm = PortScanner()
-    if type(target) is list:
+    if isinstance(target, list):
         target = listToString(target)
     if mode == ScanMode.Evade:
-        resp = nm.scan(hosts=target, arguments="-sn -PR -T 2 -f -g 53 --data-length 10")
+        resp = nm.scan(
+            hosts=target,
+            arguments="-sn -PR -T 2 -f -g 53 --data-length 10"
+        )
     else:
         resp = nm.scan(hosts=target, arguments="-sn -PR")
+
     return nm.all_hosts()
+
 
 #run a port scan on target using nmap
 def PortScan(target, scanspeed=5, mode=ScanMode.Normal, customflags=""):
-    banner("Running a portscan on host " + str(target) + "...", colors.green)
+    banner("Running a portscan on host " + str(target) + "...", bcolors.green)
 
     nm = PortScanner()
 
     try:
         if is_root():
             if mode == ScanMode.Evade:
-                resp = nm.scan(hosts=target, arguments="-sS -sV -O -Pn -T 2 -f -g 53 --data-length 10 %s" % (customflags))
+                resp = nm.scan(
+                    hosts=target,
+                    arguments=" ".join(
+                        [
+                            "-sS",
+                            "-sV",
+                            "-O",
+                            "-Pn",
+                            "-T",
+                            "2",
+                            "-f",
+                            "-g",
+                            "53",
+                            "--data-length",
+                            "10",
+                            customflags,
+                        ]
+                    )
+                )
             else:
-                resp = nm.scan(hosts=target, arguments="-sS -sV --host-timeout 60 -Pn -O -T %d %s" % (scanspeed, customflags))
+                resp = nm.scan(
+                    hosts=target,
+                    arguments=" ".join(
+                        [
+                            "-sS",
+                            "-sV",
+                            "--host-timeout",
+                            "60",
+                            "-Pn",
+                            "-O",
+                            "-T",
+                            str(scanspeed),
+                            customflags,
+                        ]
+                    )
+                )
         else:
-            resp = nm.scan(hosts=target, arguments="-sV --host-timeout 60 -Pn -T %d %s" % (scanspeed, customflags))
+            resp = nm.scan(
+                hosts=target,
+                arguments=" ".join(
+                    [
+                        "-sV",
+                        "--host-timeout",
+                        "60",
+                        "-Pn",
+                        "-T",
+                        str(scanspeed),
+                        customflags,
+                    ]
+                )
+            )
     except Exception as e:
-        error("Error: %s" % (e))
-        exit(0)
+        raise SystemExit(f"Error: {e}")
+    else:
+        return nm
 
-    return nm
 
 def CreateNoise(target):
     nm = PortScanner()
@@ -134,23 +182,23 @@ def CreateNoise(target):
             while True:
                 resp = nm.scan(hosts=target, arguments="-A -T 5")
     except KeyboardInterrupt:
-        pass
+        raise SystemExit("Ctr+C, aborting.")
+
 
 def NoiseScan(target, scantype=ScanType.ARP, timeout=None):
-    banner("Creating noise...", colors.green)
+    banner("Creating noise...", bcolors.green)
 
-    if scantype == ScanType.Ping:
-        Uphosts = TestPing(target)
-    elif scantype == ScanType.ARP:
-        if is_root():
-            Uphosts = TestArp(target)
-        else:
-            Uphosts = TestPing(target)
-    
+    Uphosts = TestPing(target)
+    match scantype:
+        case ScanType.ARP:
+            if is_root():
+                Uphosts = TestArp(target)
+
     NoisyProcesses = []
-
     for host in Uphosts:
-        info("Started creating noise on %s..." % (host))
+        log.logger(
+            "info", f"Started creating noise on {host}..."
+        )
         P = Process(target=CreateNoise, args=(host,))
         NoisyProcesses.append(P)
         P.start()
@@ -187,7 +235,7 @@ def DiscoverHosts(target, scantype=ScanType.ARP, scanspeed=3, mode=ScanMode.Norm
         banner("Scanning " + str(len(target)) + " target(s) using " + str(scantype.name) + " scan...", colors.green)
     else:
         banner("Scanning " + target + " using " + str(scantype.name) + " scan...", colors.green)
-    
+
     if scantype == ScanType.Ping:
         OnlineHosts = TestPing(target, mode)
         return OnlineHosts
@@ -266,12 +314,12 @@ def AnalyseScanResults(nm, target=None):
         error("Target " + str(target) + " seems to have no open ports.")
         return HostArray
     for port in nm[target]['tcp'].keys():
-                        
+
         if not len(nm[str(target)]['tcp'][int(port)]['state']) == 0:
             state = nm[str(target)]['tcp'][int(port)]['state']
         else:
             state = 'Unknown'
-    
+
         if not len(nm[str(target)]['tcp'][int(port)]['name']) == 0:
             service = nm[str(target)]['tcp'][int(port)]['name']
         else:
@@ -289,7 +337,7 @@ def AnalyseScanResults(nm, target=None):
 
         println(
             (
-                bcolors.cyan + "Port : " + bcolors.endc + "{0:10}" + 
+                bcolors.cyan + "Port : " + bcolors.endc + "{0:10}" +
                 bcolors.cyan + " State : " + bcolors.endc + "{1:10}" +
                 bcolors.cyan + " Service : " + bcolors.endc + "{2:15}" +
                 bcolors.cyan + " Product : " + bcolors.endc + "{3:20}" +
