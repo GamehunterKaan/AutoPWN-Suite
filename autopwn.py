3bbfe0aaf8c4442b459956e389f474345fb2f550
GamehunterKaan <kaan007.gultekin@gmail.com>
2022-06-22 14:25:17 +0300

    Fixed #19

--- autopwn.py
+++ autopwn.py
@@ -23,7 +23,7 @@ except ImportError as e:
     exit(1)
 
 __author__ = 'GamehunterKaan'
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 
 #parse command line arguments
 argparser = ArgumentParser(description="AutoPWN Suite")
