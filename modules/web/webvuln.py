55666bd8d3bfe6be9d69ca518ff5115be3dd6e61
iaacornus <iaacornus.devel@gmail.com>
2022-06-23 20:52:32 +0800

    turned webvlun into higher order function and some rewrites

--- modules/web/webvuln.py
+++ modules/web/webvuln.py
@@ -3,41 +3,40 @@ from modules.logger import error, banner, colors
 from modules.web.crawler import crawl
 from modules.web.lfi import test_lfi
 
-def get_url(target):
-    """
-    Get the target url
-    """
-    try:
-        request = get("http://" + target, timeout=10)
-        if request.status_code == 200:
-            return "http://" + target
-    except Exception as e:
-        try:
-            request = get("https://" + target, timeout=10)
-            if request.status_code == 200:
-                return "https://" + target
-        except Exception as e:
-            error("Could not get url for " + target)
-    return None
 
 def webvuln(target):
     """
     Test for web vulnerabilities
     """
+    def get_url(target):
+        """
+        Get the target url
+        """
+        url_ = f"http://{target}"
+        for _ in range(3):
+            try:
+                get(url_, timeout=10)
+            except Exception as e:
+                continue
+            else:
+                return url_
+
+        return None
+
     target_url = get_url(target)
     if target_url is None:
         return
     #banner("got url " + target_url)
-    banner("Testing web application on " + target + "...", colors.purple)
+    banner(f"Testing web application on {target} ...", colors.purple)
     # crawl the target_url
     urls = crawl(target_url)
     # test for lfi
     tested_urls = []
     for url in urls:
         if "?" in url:
-            print("Testing for LFI on " + url, end="\r")
+            print(f"Testing for LFI on {url}", end="\r")
             tested_urls.append(url)
             test_lfi(url)
-    
+
     if len(tested_urls) == 0:
         error("No testable URLs found")
