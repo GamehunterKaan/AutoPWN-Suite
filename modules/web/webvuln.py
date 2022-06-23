9e047c7de82a83f653f93f6a643282072ed7b6df
iaacornus <iaacornus.devel@gmail.com>
2022-06-23 21:00:38 +0800

    synched with changes from modules.web.lfi

--- modules/web/webvuln.py
+++ modules/web/webvuln.py
@@ -1,13 +1,15 @@
 from requests import get
 from modules.logger import error, banner, colors
 from modules.web.crawler import crawl
-from modules.web.lfi import test_lfi
+from modules.web.lfi import TestLFI
 
 
 def webvuln(target):
     """
     Test for web vulnerabilities
     """
+    test_lfi = TestLFI()
+
     def get_url(target):
         """
         Get the target url
@@ -24,19 +26,18 @@ def webvuln(target):
         return None
 
     target_url = get_url(target)
-    if target_url is None:
-        return
-    #banner("got url " + target_url)
-    banner(f"Testing web application on {target} ...", colors.purple)
-    # crawl the target_url
-    urls = crawl(target_url)
-    # test for lfi
-    tested_urls = []
-    for url in urls:
-        if "?" in url:
-            print(f"Testing for LFI on {url}", end="\r")
-            tested_urls.append(url)
-            test_lfi(url)
+    if target_url is not None:
+        #banner("got url " + target_url)
+        banner(f"Testing web application on {target} ...", colors.purple)
+        # crawl the target_url
+        urls = crawl(target_url)
+        # test for lfi
+        tested_urls = []
+        for url in urls:
+            if "?" in url:
+                print(f"Testing for LFI on {url}", end="\r")
+                tested_urls.append(url)
+                test_lfi.test_lfi(url)
 
-    if len(tested_urls) == 0:
-        error("No testable URLs found")
+        if len(tested_urls) == 0:
+            error("No testable URLs found")
