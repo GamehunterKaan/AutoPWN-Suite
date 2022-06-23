7ea968b2b1d2af24defbf8a33b694e9982d62f98
iaacornus <iaacornus.devel@gmail.com>
2022-06-23 14:56:30 +0800

    refactoring and simplified some stuffs

--- modules/logger.py
+++ modules/logger.py
@@ -1,73 +1,92 @@
 from platform import system as system_name
 
+
 if system_name() == "Windows":
     from modules.colorama import init
     init()
 
+
 def InitializeLogger(context):
     global outfile
     outfile = context
-    file = open(outfile, 'w')
-    file.close()
+    with open(outfile, "w", encoding="utf-8") as file:
+        file.close()
+
 
 # https://stackoverflow.com/a/287944
 class bcolors:
-    header = '\033[95m'
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    green = '\033[92m'
-    yellow = '\033[93m'
-    red = '\033[91m'
-    purple = '\033[38;5;93m'
-    endc = '\033[0m'
-    bold = '\033[1m'
-    underline = '\033[4m'
+    header = "\033[95m"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
+    purple = "\033[38;5;93m"
+    endc = "\033[0m"
+    bold = "\033[1m"
+    underline = "\033[4m"
+
 
 class colors:
-    blue = 'blue'
-    cyan = 'cyan'
-    green = 'green'
-    yellow = 'yellow'
-    red = 'red'
-    purple = 'purple'
-    bold = 'bold'
-    underline = 'underline'
-    no_new_line = 'no_new_line'
-    endc = 'endc'
+    blue = "blue"
+    cyan = "cyan"
+    green = "green"
+    yellow = "yellow"
+    red = "red"
+    purple = "purple"
+    bold = "bold"
+    underline = "underline"
+    no_new_line = "no_new_line"
+    endc = "endc"
+
 
 def WriteToFile(data):
-    colors = [bcolors.blue, bcolors.red, bcolors.yellow, bcolors.green, bcolors.cyan, bcolors.endc, bcolors.bold, bcolors.underline]
+    colors = [
+            bcolors.blue,
+            bcolors.red,
+            bcolors.yellow,
+            bcolors.green,
+            bcolors.cyan,
+            bcolors.endc,
+            bcolors.bold,
+            bcolors.underline
+        ]
+
     for colorcode in colors:
         data = data.replace(colorcode, "")
+
     filename = outfile
-    if system_name() == "Windows":
-        data = data.encode("utf-8")
-        file = open(filename, 'ab')
-        file.write(b"\n" + bytes(data))
-    else:
-        file = open(filename, "a")
-        file.write("\n" + data)
-    file.close()
+    with open(filename, "ab", encoding="utf-8") as file:
+        if system_name() == "Windows":
+            file.write(b"\n" + bytes(data))
+        else:
+            file.write(f"\n{data}")
+
 
 def info(msg):
-    print(bcolors.blue + "[+] " + bcolors.endc + str(msg))
-    WriteToFile("[+] " + msg)
+    print(f"{bcolors.blue}[+] {bcolors.endc} {msg}")
+    WriteToFile(f"[+] {msg}")
+
 
 def error(msg):
-    print(bcolors.red + "[-] " + bcolors.endc + str(msg))
-    WriteToFile("[-] " + msg)
+    print(f"{bcolors.blue}[-] {bcolors.endc} {msg}")
+    WriteToFile(f"[-] {msg}")
+
 
 def warning(msg):
-    print(bcolors.yellow + "[*] " + bcolors.endc + str(msg))
-    WriteToFile("[*] " + msg)
+    print(f"{bcolors.blue}[*] {bcolors.endc} {msg}")
+    WriteToFile(f"[*] {msg}")
+
 
 def success(msg):
-    print(bcolors.green + "[+] " + bcolors.endc + str(msg))
-    WriteToFile("[+] " + msg)
+    print(f"{bcolors.blue}[+] {bcolors.endc} {msg}")
+    WriteToFile(f"[+] {msg}")
+
 
 def println(msg):
     print(msg)
     WriteToFile(msg)
+
 
 def banner(msg, color):
     print_colored("\n" + "─" * 60, color)
@@ -77,22 +96,23 @@ def banner(msg, color):
     WriteToFile((msg).center(60))
     WriteToFile("─" * 60 + "\n")
 
+
 def print_colored(text,color):
-    if color == 'blue':
-        print(bcolors.blue + str(text) + bcolors.endc)
-    elif color == 'cyan':
-        print(bcolors.cyan + str(text) + bcolors.endc)
-    elif color == 'green':
-        print(bcolors.green + str(text) + bcolors.endc)
-    elif color == 'yellow':
-        print(bcolors.yellow + str(text) + bcolors.endc)
-    elif color == 'red':
-        print(bcolors.red + str(text) + bcolors.endc)
+    if color == "blue":
+        print(f"{bcolors.blue} {text} {bcolors.endc}")
+    elif color == "cyan":
+        print(f"{bcolors.cyan} {text} {bcolors.endc}")
+    elif color == "green":
+        print(f"{bcolors.green} {text} {bcolors.endc}")
+    elif color == "yellow":
+        print(f"{bcolors.yellow} {text} {bcolors.endc}")
+    elif color == "red":
+        print(f"{bcolors.red} {text} {bcolors.endc}")
     elif color == "purple":
-        print(bcolors.purple + str(text) + bcolors.endc)
-    elif color == 'bold':
-        print(bcolors.bold + str(text) + bcolors.endc)
-    elif color == 'underline':
-        print(bcolors.underline + str(text) + bcolors.endc)
-    elif color == 'no_new_line':
-        print(str(text), end='')
+        print(f"{bcolors.purple} {text} {bcolors.endc}")
+    elif color == "bold":
+        print(f"{bcolors.bold} {text} {bcolors.endc}")
+    elif color == "underline":
+        print(f"{bcolors.underline} {text} {bcolors.endc}")
+    elif color == "no_new_line":
+        print(text, end="")
