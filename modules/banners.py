11e963e6a3490171a4631cf03f0f920fbefff76f
iaacornus <iaacornus.devel@gmail.com>
2022-06-24 14:48:17 +0800

    removed unnecessary logging and simplified the banner print method

--- modules/banners.py
+++ modules/banners.py
@@ -1,24 +1,27 @@
-from modules.logger import println, warning, info
-from platform import system as system_name
+from os import get_terminal_size
 
-if system_name() == "Windows":
-    from modules.colorama import init
-    init()
+from rich.console import Console
+
 
 # https://patorjk.com/software/taag/
 def print_banner():
+    console = Console()
+
     banner = """\033[0m│        \033[94m___           __          ____  _       __ _   __   _____         _  __\033[0m         │
 │       \033[94m/   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___\033[0m    │
 │      \033[94m/ /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\\033[0m   │
 │     \033[94m/ ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/\033[0m   │
 │    \033[94m/_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/\033[0m    │
 │                                                                                        │"""
+    w, _ = get_terminal_size()
 
-    info("Developed by GamehunterKaan. (https://auto.pwnspot.com)")
-    println("\n" + "┌" + "─" * 88 + "┐")
-    println(banner.center(90))
-    println("└" + "─" * 88 + "┘" + "\n")
-    warning(
-        "I am not responsible if you are doing "
-        + "something illegal using this program! \n"
+    console.print(
+        "Developed by GamehunterKaan. (https://auto.pwnspot.com)", style="blue"
+    )
+    print("\n┌" + "─"*w + "┐")
+    print(banner.center(w))
+    print("└" + "─"*w + "┘\n")
+    console.print(
+        "I am not responsible if you are doing something"
+        + " illegal using this program! \n", style="red"
     )
