33c876e357d77d4009f378747860f790455d9ab8
iaacornus <iaacornus.devel@gmail.com>
2022-06-24 14:40:01 +0800

    moved to another file to maintain a cleanliness of logger.py

---
+++ modules/colors.py
@@ -0,0 +1,24 @@
+class bcolors:
+    header = "\033[95m"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
+    purple = "\033[38;5;93m"
+    endc = "\033[0m"
+    bold = "\033[1m"
+    underline = "\033[4m"
+
+
+class colors:
+    blue = "blue"
+    cyan = "cyan"
+    green = "green"
+    yellow = "yellow"
+    red = "red"
+    purple = "purple"
+    bold = "bold"
+    underline = "underline"
+    no_new_line = "no_new_line"
+    endc = "endc"
