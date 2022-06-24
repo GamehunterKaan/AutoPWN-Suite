80be2127c0b6b151d035d6cc63f8df74ce2f51b1
James Aaron Erang <96870156+iaacornus@users.noreply.github.com>
2022-06-25 02:05:07 +0800

    Revert "Logging rewrite "

--- autopwn.py
+++ autopwn.py
@@ -1,3 +1,4 @@
+from dis import dis
 import distro
 from sys import platform as sys_platform
 from platform import platform, system
@@ -9,11 +10,8 @@ from subprocess import check_call, CalledProcessError, DEVNULL
 from datetime import datetime
 
 from rich.console import Console
-from rich.text import Text
 from configparser import ConfigParser
 
-from modules.logger import Logger
-from modules.colors import bcolors
 from modules.scanner import is_root
 from modules.banners import print_banner
 from modules.getexploits import GetExploitsFromArray
@@ -33,6 +31,15 @@ from modules.scanner import (
     ScanType,
     NoiseScan
 )
+from modules.logger import (
+    info,
+    error,
+    warning,
+    println,
+    InitializeLogger,
+    bcolors
+)
+
 
 __author__ = "GamehunterKaan"
 __version__ = "1.5.1"
@@ -207,7 +214,6 @@ reportargs.add_argument(
 
 args = argparser.parse_args()
 console = Console()
-log = Logger()
 
 
 def InitArgsConf():
@@ -234,10 +240,7 @@ def InitArgsConf():
             try:
                 args.speed = int(config.get("AUTOPWN", "speed"))
             except ValueError:
-                log.logger(
-                    "error",
-                    "Invalid speed value in config file. (Default : 3)"
-                )
+                error("[!] Invalid speed value in config file. (Default : 3)")
 
         if config.has_option("AUTOPWN", "apikey"):
             args.api = config.get("AUTOPWN", "apikey").lower()
@@ -285,13 +288,11 @@ def InitArgsConf():
             args.reportwebhook = config.get("REPORT", "webhook").lower()
 
     except FileNotFoundError:
-        log.logger("error", "Config file not found!")
-        raise SystemExit
+        raise SystemExit("Config file not found!")
     except PermissionError:
-        log.logger(
-            "error", "Permission denied while trying to read config file!"
+        raise SystemExit(
+            "Permission denied while trying to read config file!"
         )
-        raise SystemExit
 
 
 def InitArgsScanType():
@@ -315,17 +316,17 @@ def InitArgsAPI():
             with open("api.txt", "r", encoding="utf-8") as f:
                 apiKey = f.readline().strip("\n")
         except FileNotFoundError:
-            log.logger(
-                "warning",
+            warning(
                 "No API key specified and no api.txt file found. "
                 + "Vulnerability detection is going to be slower!"
-                + "You can get your own NIST API key from "
+            )
+            warning(
+                "You can get your own NIST API key from "
                 + "https://nvd.nist.gov/developers/request-an-api-key"
             )
+
         except PermissionError:
-            log.logger(
-                "error", "Permission denied while trying to read api.txt!"
-            )
+            error("Permission denied while trying to read api.txt!")
 
     return apiKey
 
@@ -410,10 +411,9 @@ def install_nmap_linux():
             elif _distro_ == 5:
                 distro_ = "opensuse"
             else:
-                log.logger("error", "Couldn't install nmap (Linux)")
+                error("Couldn't install nmap! (Linux)")
                 break
             continue
-
 
 def install_nmap_windows():
     # TODO: implement this
@@ -430,7 +430,7 @@ def install_nmap_windows():
             stderr=DEVNULL
         )
     except CalledProcessError:
-        log.logger("error", "Couldn't install nmap! (Windows)")
+        error("Couldn't install nmap! (Windows)")
 
 
 def install_nmap_mac():
@@ -445,7 +445,7 @@ def install_nmap_mac():
             stderr=DEVNULL
         )
     except CalledProcessError:
-        log.logger("error", "Couldn't install nmap! (Mac)")
+        error("Couldn't install nmap! (Mac)")
 
 
 def check_nmap():
@@ -457,9 +457,9 @@ def check_nmap():
             stderr=DEVNULL
         )
     except FileNotFoundError:
-        log.logger("warning", "Nmap is not installed.")
+        warning("Nmap is not installed.")
         auto_install = input(
-                f"Install Nmap on your system ({distro.id()}: {platform()})? "
+                f"Install Nmap on your system ({distro.id()}: {platform})? "
             )
         if auto_install in ["y", "Y"]:
             platform_ = system().lower()
@@ -474,7 +474,7 @@ def check_nmap():
                     "Unknown OS! Auto installation not supported!"
                 )
         else:
-            log.logger("error", "Denied permission to install Nmap.")
+            error("Denied permission to install Nmap.")
             raise SystemExit
 
 
@@ -504,16 +504,11 @@ def InitArgsTarget():
                 with open(args.hostfile, "r", encoding="utf-8") as target_file:
                     target = target_file.readlines()
             except FileNotFoundError:
-                log.logger("error", "Host file not found!")
+                error("Host file not found!")
             except PermissionError:
-                log.logger(
-                    "error",
-                    "Permission denied while trying to read host file!"
-                )
+                error("Permission denied while trying to read host file!")
             except Exception:
-                log.logger(
-                    "error", "Unknown error while trying to read host file!"
-                )
+                error("Unknown error while trying to read host file!")
             else:
                 return target
 
@@ -536,16 +531,15 @@ def InitArgsMode():
     if args.mode == "evade":
         if is_root():
             scanmode = ScanMode.Evade
-            log.logger("info", "Evasion mode enabled!")
+            info("Evasion mode enabled!")
         else:
-            log.logger(
-                "error",
-                "You must be root to use evasion mode!"
-                + " Switching back to normal mode ..."
+            error(
+                "You must be root to use evasion "
+                + "mode! Switching back to normal mode..."
             )
     elif args.mode == "noise":
         scanmode = ScanMode.Noise
-        log.logger("error", "Noise mode enabled!")
+        warning("Noise mode enabled!")
 
     return scanmode
 
@@ -585,9 +579,9 @@ def InitReport():
                     "Enter the email server to send the report from : "
                 )
             if ReportMailServer == "smtp.gmail.com":
-                log.logger(
-                    "error",
-                    "Google no longer supports sending mails via SMTP."
+                error(
+                    "Google no longer supports sending mails"
+                    + " via SMTP! Canceling report via email."
                 )
                 return ReportType.NONE, None
 
@@ -600,7 +594,8 @@ def InitReport():
                     )
                 if isinstance(ReportMailPort, int):
                     break
-                log.logger("error", "Invalid port number!")
+
+                error("Invalid port number!")
 
         EmailObj = ReportMail(
                 ReportEmail,
@@ -635,7 +630,7 @@ def ParamPrint(term_width):
         console.print("\n┌─[ Scanning with the following parameters. ]")
 
     console.print(
-        "├" + "─" * (term_width-1)
+        "├" + "─" * term_width
         + f"\n│\tTarget : {str(targetarg)}\n"
         + f"│\tScan type : {str(scantype.name)}\n"
         + f"│\tScan mode : {str(scanmode.name)}\n"
@@ -646,7 +641,7 @@ def ParamPrint(term_width):
         + f"│\tDont ask for confirmation : {str(DontAskForConfirmation)}\n"
         + f"│\tHost file : {str(args.hostfile)}\n"
         + f"│\tReporting method : {str(args.report)}\n"
-        + "└" + "─" * (term_width-1)
+        + "└" + "─" * term_width
     )
 
 
@@ -695,8 +690,9 @@ def GetHostsToScan(hosts):
 
     index = 0
     for host in hosts:
-        console.print(
-            Text(f"[red][[/red]{index}[red]][/red] {host}", justify="center")
+        println(
+            str(f"{bcolors.red}[{bcolors.endc}{index}"
+            + f"{bcolors.red}]{bcolors.endc} {host}").center(60)
         )
         index += 1
 
@@ -766,6 +762,8 @@ def main():
         console.print(f"AutoPWN Suite v{__version__}")
         raise SystemExit
 
+    print_banner()
+
     if args.config:
         InitArgsConf()
 
@@ -773,6 +771,7 @@ def main():
     global outputfile, DontAskForConfirmation, hostfile, noisetimeout
 
     outputfile = args.output
+    InitializeLogger(outputfile)
     print_banner()
 
     width_, _ = get_terminal_size()
@@ -788,10 +787,9 @@ def main():
     ReportMethod, ReportObject = InitReport()
 
     if not is_root():
-        log.logger(
-            "error",
-            "It is recommended to run this script as root"
-            + "since it is more silent and accurate."
+        error(
+            "It's recommended to run this script as "
+            + "root since it's more silent and accurate."
         )
 
     ParamPrint(width_)
--- modules/logger.py
+++ modules/logger.py
@@ -1,73 +1,118 @@
-import logging
-from os import get_terminal_size
+from platform import system as system_name
 
-from rich.logging import RichHandler
-from rich.console import Console
-from rich.text import Text
+
+if system_name() == "Windows":
+    from modules.colorama import init
+    init()
+
+
+def InitializeLogger(context):
+    global outfile
+    outfile = context
+    with open(outfile, "w", encoding="utf-8") as file:
+        file.close()
+
+
+# https://stackoverflow.com/a/287944
+class bcolors:
+    header = "\033[95m"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
+    purple = "\033[38;5;93m"
+    endc = "\033[0m"
+    bold = "\033[1m"
+    underline = "\033[4m"
+
+
+class colors:
+    blue = "blue"
+    cyan = "cyan"
+    green = "green"
+    yellow = "yellow"
+    red = "red"
+    purple = "purple"
+    bold = "bold"
+    underline = "underline"
+    no_new_line = "no_new_line"
+    endc = "endc"
+
+
+def WriteToFile(data):
+    colors = [
+            bcolors.blue,
+            bcolors.red,
+            bcolors.yellow,
+            bcolors.green,
+            bcolors.cyan,
+            bcolors.endc,
+            bcolors.bold,
+            bcolors.underline
+        ]
+
+    for colorcode in colors:
+        data = data.replace(colorcode, "")
+
+    filename = outfile
+    with open(filename, "ab", encoding="utf-8") as file:
+        if system_name() == "Windows":
+            file.write(b"\n" + bytes(data))
+        else:
+            file.write(f"\n{data}")
+
+
+def info(msg):
+    print(f"{bcolors.blue}[+] {bcolors.endc} {msg}")
+    WriteToFile(f"[+] {msg}")
+
+
+def error(msg):
+    print(f"{bcolors.blue}[-] {bcolors.endc} {msg}")
+    WriteToFile(f"[-] {msg}")
+
+
+def warning(msg):
+    print(f"{bcolors.blue}[*] {bcolors.endc} {msg}")
+    WriteToFile(f"[*] {msg}")
+
+
+def success(msg):
+    print(f"{bcolors.blue}[+] {bcolors.endc} {msg}")
+    WriteToFile(f"[+] {msg}")
+
+
+def println(msg):
+    print(msg)
+    WriteToFile(msg)
 
 
 def banner(msg, color):
-    console = Console()
-    log = Logger()
-
-    term_width, _ = get_terminal_size()
-
-    console.print("-"*term_width, style=color)
-    console.print(Text(msg, justify="center"), style=color)
-    console.print("-"*term_width, style=color)
-    log.logger("info", msg, stream_=False)
+    print_colored("\n" + "─" * 60, color)
+    print_colored(str(msg).center(60), color)
+    print_colored("─" * 60 + "\n", color)
+    WriteToFile("\n" + "─" * 60)
+    WriteToFile((msg).center(60))
+    WriteToFile("─" * 60 + "\n")
 
 
-class Logger:
-    """
-    Custom logger
-    """
-
-    def __init__(self) -> None:
-        logging.basicConfig(
-            format="%(message)s",
-            level=logging.INFO,
-            datefmt="[%X]",
-            handlers=[RichHandler()]
-        )
-
-        RichHandler.KEYWORDS = [
-                "[+]",
-                "[-]",
-                "[*]"
-            ]
-        self.log: object = logging.getLogger("rich")
-        file_log: object = logging.FileHandler(filename="output.log")
-
-        file_log.setLevel(logging.INFO)
-        file_log.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
-        self.log.addHandler(file_log)
-
-    def logger(
-            self,
-            exception_: str,
-            message: str,
-        ) -> None:
-        """
-        * Log the proccesses with the passed message depending on the
-        * exception_ variable
-
-        @args
-            exception_: str, determines what type of log level to use
-                (1.) info
-                (2.) error
-                (3.) warning
-                (4.) success
-            message: str, message to be logged.
-
-        ? Returns none.
-        """
-
-        if exception_ == "info":
-            self.log.info(f"[+] {message}")
-        elif exception_ == "error":
-            self.log.warning(f"[-] {message}")
-        elif exception_ == "warning":
-            self.log.info(f"[*] {message}")
-        elif exception_ == "success":
-            self.log.info(f"[+] {message}")
+def print_colored(text,color):
+    if color == "blue":
+        print(f"{bcolors.blue} {text} {bcolors.endc}")
+    elif color == "cyan":
+        print(f"{bcolors.cyan} {text} {bcolors.endc}")
+    elif color == "green":
+        print(f"{bcolors.green} {text} {bcolors.endc}")
+    elif color == "yellow":
+        print(f"{bcolors.yellow} {text} {bcolors.endc}")
+    elif color == "red":
+        print(f"{bcolors.red} {text} {bcolors.endc}")
+    elif color == "purple":
+        print(f"{bcolors.purple} {text} {bcolors.endc}")
+    elif color == "bold":
+        print(f"{bcolors.bold} {text} {bcolors.endc}")
+    elif color == "underline":
+        print(f"{bcolors.underline} {text} {bcolors.endc}")
+    elif color == "no_new_line":
+        print(text, end="")
