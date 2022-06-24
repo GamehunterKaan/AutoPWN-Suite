ae7ef0bfe5c0abd4df1bc9156ddec1d72228fe2f
iaacornus <iaacornus.devel@gmail.com>
2022-06-25 01:50:55 +0800

    rewrite the banner and used panel, text, and align from console instead of manual formatting

--- modules/banners.py
+++ modules/banners.py
@@ -1,27 +1,41 @@
 from os import get_terminal_size
 
 from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from rich.align import Align
 
 
 # https://patorjk.com/software/taag/
 def print_banner():
     console = Console()
 
-    banner = """\033[0m│        \033[94m___           __          ____  _       __ _   __   _____         _  __\033[0m         │
-│       \033[94m/   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___\033[0m    │
-│      \033[94m/ /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\\033[0m   │
-│     \033[94m/ ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/\033[0m   │
-│    \033[94m/_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/\033[0m    │
-│                                                                                        │"""
-    w, c = get_terminal_size()
+    banner = """\
+    ___           __          ____  _       __ _   __   _____         _  __
+        /   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___
+       / /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\
+      / ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/
+     /_/  |_|\__,_/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \__,_//_/ \__/ \___/
+    """
+    width, _ = get_terminal_size()
 
-    console.print(
-        "Developed by GamehunterKaan. (https://auto.pwnspot.com)", style="blue"
+
+    panel = Panel(
+        Align(
+            Text(
+                banner,
+                justify="center",
+                style="blue"
+            ),
+            vertical="middle", align="center"
+        ),
+        width=width, height=8
     )
-    print("\n┌" + "─"*w + "┐")
-    print(banner.center(c))
-    print("└" + "─"*w + "┘\n")
+    console.print(
+        "[blue]Developed by GamehunterKaan. (https://auto.pwnspot.com)[/blue]"
+    )
+    console.print(panel)
     console.print(
         "I am not responsible if you are doing something"
-        + " illegal using this program! \n", style="red"
+        + " illegal using this program!", style="red"
     )
