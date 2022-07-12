756da0b9964f40be05e57da11b48ed2fdb773263
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-13 00:02:55 +0300

    Few changes

--- modules/web/webvuln.py
+++ modules/web/webvuln.py
@@ -1,30 +1,30 @@
-from requests import get
-
 from modules.logger import banner
 from modules.web.crawler import crawl
 from modules.web.lfi import TestLFI
+from requests import get
 
 
-def webvuln(target, log):
+def webvuln(target, log, console) -> None:
     """
     Test for web vulnerabilities
     """
 
-    test_lfi = TestLFI(log)
+    LFI = TestLFI(log, console)
 
     def get_url(target):
         """
         Get the target url
         """
 
-        url_ = f"http://{target}"
-        for _ in range(3): # do 3 tries
-            try:
-                get(url_, timeout=10)
-            except ConnectionError:
-                continue
-            else:
-                return url_
+        url_ = [f"http://{target}", f"https://{target}"]
+        for url in url_:
+            for _ in range(3): # do 3 tries
+                try:
+                    get(url, timeout=10)
+                except Exception as e:
+                    continue
+                else:
+                    return url
 
         return None
 
@@ -33,14 +33,29 @@ def webvuln(target, log):
     if target_url is None:
         return
 
-    banner(f"Testing web application on {target} ...", "purple")
+
     urls = crawl(target_url, log)
     tested_urls = []
+    testable_urls = []
     for url in urls: # test for lfi
         if "?" in url:
-            print(f"Testing for LFI on {url}", end="\r")
-            test_lfi(url)
-            tested_urls.append(url)
+            testable_urls.append(url)
 
-    if len(tested_urls) == 0:
-        log.logger("error", "No testable URLs found")
+    log.logger(
+        "info", 
+        f"Found {len(testable_urls)} testable urls."
+    )
+
+    if len(testable_urls) == 0:
+        log.logger(
+            "error",
+            "No testable urls found for current host."
+        )
+        return
+
+    banner(f"Testing web application on {target} ...", "purple", console)
+
+    for url in testable_urls:
+        print(f"Testing for LFI on {url}", end="\r")
+        LFI.test_lfi(url, console)
+        tested_urls.append(url)
