4ffec34ea261a82a3dbfa11f0d8d4d3c6fc28f10
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-13 00:02:05 +0300

    Bugfix

--- modules/web/crawler.py
+++ modules/web/crawler.py
@@ -1,21 +1,19 @@
+from bs4 import BeautifulSoup
+from modules.random_user_agent import random_user_agent
 from requests import get
 
-from bs4 import BeautifulSoup
 
-from modules.random_user_agent import random_user_agent
-
-
-def crawl(target_url, log):
+def crawl(target_url, log) -> set:
     if not target_url.endswith("/"):
         target_url += "/"
 
     for tries in range(3):
         try:
             reqs = get(
-                    target_url, headers={
-                            "User-Agent": next(random_user_agent())
-                        }
-                )
+                target_url, headers={
+                    "User-Agent": next(random_user_agent(log))
+                }
+            )
             soup = BeautifulSoup(reqs.text, "html.parser")
         except ConnectionError:
             if tries < 3:
@@ -27,14 +25,20 @@ def crawl(target_url, log):
             else:
                 log.logger(
                     "error",
-                    f"Connection error raised, reached a maximum of 3 tries."
+                    "Connection error raised, reached a maximum of 3 tries."
                 )
                 raise SystemExit
+
+    log.logger(
+        "info", 
+        f"Crawling web application at {target_url} ..."
+    )
 
     urls = set()
     for link in soup.find_all("a", href=True):
         url = link["href"]
-
+        if url == None:
+            continue
         if not url.startswith("http"):
             if "#" in url or url is None or url == "":
                 continue
@@ -51,6 +55,9 @@ def crawl(target_url, log):
             if url.startswith(target_url):
                 if url not in urls:
                     urls.add(url)
+
+    temp_urls = set()
+    # ^^^ to prevent "RuntimeError: Set changed size during iteration"
 
     if len(urls) < 10:
         for each_url in urls:
@@ -78,10 +85,12 @@ def crawl(target_url, log):
                             url = f"{each_url}{url}"
 
                         if url not in urls:
-                            urls.add(url)
+                            temp_urls.add(url)
                     else:
                         if url.startswith(each_url):
                             if url not in urls:
-                                urls.add(url)
+                                temp_urls.add(url)
+    for url in temp_urls:
+        urls.add(url)
 
     return urls
