bf6311b310dbe935145f2b2fbc934b208e2f43f9
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-12 23:58:42 +0300

    Removed file handler. logger will no longer handle messages that should be logged into a file

--- modules/logger.py
+++ modules/logger.py
@@ -2,20 +2,17 @@ import logging
 from os import get_terminal_size
 
 from rich.logging import RichHandler
-from rich.console import Console
 from rich.text import Text
 
 
-def banner(msg, color):
-    console = Console()
+def banner(msg, color, console) -> None:
     log = Logger()
 
     term_width, _ = get_terminal_size()
 
     console.print("─"*term_width, style=color)
-    console.print(Text(msg, justify="center"), style=color)
+    console.print(Text(msg), justify="center", style=color)
     console.print("─"*term_width, style=color)
-    log.logger("info", msg)
 
 
 class Logger:
@@ -23,7 +20,7 @@ class Logger:
     Custom logger
     """
 
-    def __init__(self, filename: str = None) -> None:
+    def __init__(self) -> None:
         logging.basicConfig(
             format="%(message)s",
             level=logging.INFO,
@@ -36,15 +33,9 @@ class Logger:
                 "[-]",
                 "[*]"
             ]
+
         self.log: object = logging.getLogger("rich")
 
-        file_log: object = logging.FileHandler(filename="output.log")
-        if filename is not None:
-            file_log: object = logging.FileHandler(filename="autopwn.log")
-
-        file_log.setLevel(logging.INFO)
-        file_log.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
-        self.log.addHandler(file_log)
 
     def logger(
             self,
