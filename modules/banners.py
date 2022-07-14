f9906d53d36ebfc1f9369402129aeb3f465686b2
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-14 11:18:18 +0300

    Added smaller banner for smaller terminals

--- modules/banners.py
+++ modules/banners.py
@@ -1,20 +1,35 @@
-from os import get_terminal_size
-
 from rich.align import Align
 from rich.panel import Panel
 from rich.text import Text
 
+from modules.utils import get_terminal_width
+
+
+
 
 # https://patorjk.com/software/taag/
 def print_banner(console) -> None:
+    width = get_terminal_width()
+    height = 8
     banner = """\
-    ___           __          ____  _       __ _   __   _____         _  __
-        /   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___
-       / /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\
-      / ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/
-     /_/  |_|\____/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \____//_/ \__/ \___/
-    """
-    width, _ = get_terminal_size()
+___           __          ____  _       __ _   __   _____         _  __
+    /   |  __  __ / /_ ____   / __ \| |     / // | / /  / ___/ __  __ (_)/ /_ ___
+   / /| | / / / // __// __ \ / /_/ /| | /| / //  |/ /   \__ \ / / / // // __// _ \\
+  / ___ |/ /_/ // /_ / /_/ // ____/ | |/ |/ // /|  /   ___/ // /_/ // // /_ /  __/
+ /_/  |_|\____/ \__/ \____//_/      |__/|__//_/ |_/   /____/ \____//_/ \__/ \___/
+"""
+
+
+    banner_small = """\
+╔═╗┬ ┬┌┬┐┌─┐╔═╗╦ ╦╔╗╔  ╔═╗┬ ┬┬┌┬┐┌─┐
+╠═╣│ │ │ │ │╠═╝║║║║║║  ╚═╗│ ││ │ ├┤ 
+╩ ╩└─┘ ┴ └─┘╩  ╚╩╝╝╚╝  ╚═╝└─┘┴ ┴ └─┘
+"""
+
+
+    if width < 90:
+        banner = banner_small
+        height = 5
 
     panel = Panel(
         Align(
@@ -25,6 +40,6 @@ def print_banner(console) -> None:
             ),
             vertical="middle", align="center"
         ),
-        width=width, height=8, subtitle="by GamehunterKaan (https://auto.pwnspot.com)"
+        width=width, height=height, subtitle="by GamehunterKaan (https://auto.pwnspot.com)"
     )
     console.print(panel)
