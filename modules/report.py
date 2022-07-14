9163441bbba8a19155a6ce9b40edfcf7e9dda013
GamehunterKaan <GamehunterKaan@users.noreply.github.com>
2022-07-14 02:41:26 -0700

    Removed unnecessary imports

--- modules/report.py
+++ modules/report.py
@@ -1,6 +1,4 @@
 from dataclasses import dataclass
-from email import encoders
-from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from enum import Enum
