2a8c114dfeed383bf60ce507b360dfd11cfa547c
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-16 15:27:21 +0300

    Simplified crawler function

--- modules/web/crawler.py
+++ modules/web/crawler.py
@@ -7,42 +7,53 @@ def crawl(target_url, log) -> set:
     if not target_url.endswith("/"):
         target_url += "/"
 
-    for tries in range(3):
-        try:
-            reqs = get(
-                target_url, headers={
+    try:
+        get(
+            target_url, headers={
+                "User-Agent": next(random_user_agent(log))
+            }
+        )
+    except ConnectionError:
+        log.logger(
+            "error",
+            f"Connection error raised."
+        )
+        return set()
+
+    log.logger("info", f"Crawling web application at {target_url} ...")
+
+    urls = link_finder(target_url, log)
+    if len(urls) < 25:
+        temp_urls = set()
+        for url in urls:
+            new_urls = link_finder(url, log)
+            for new_url in new_urls:
+                temp_urls.add(new_url)
+        
+        for url in temp_urls:
+            urls.add(url)
+
+    return urls
+
+
+def link_finder(target_url, log):
+    if not target_url.endswith("/"):
+        target_url += "/"
+
+    urls = set()
+
+    reqs = get(
+        target_url, headers={
                     "User-Agent": next(random_user_agent(log))
                 }
             )
-            soup = BeautifulSoup(reqs.text, "html.parser")
-        except ConnectionError:
-            if tries < 3:
-                log.logger(
-                    "error",
-                    f"Connection error raised, retrying again: {tries}."
-                )
-                continue
-            else:
-                log.logger(
-                    "error",
-                    "Connection error raised, reached a maximum of 3 tries."
-                )
-                raise SystemExit
-
-    log.logger(
-        "info", 
-        f"Crawling web application at {target_url} ..."
-    )
-
-    urls = set()
+    soup = BeautifulSoup(reqs.text, "html.parser")
     for link in soup.find_all("a", href=True):
         url = link["href"]
-        if url == None:
+        if url == None or url == "" or "#" in url:
             continue
         if not url.startswith("http"):
-            if "#" in url or url is None or url == "":
-                continue
-            elif url.startswith("./"):
+            if url.startswith("./"):
                 url = f"{target_url}{url.lstrip('./')}"
             elif url.startswith("/"):
                 url = f"{target_url}{url.lstrip('/')}"
@@ -55,42 +66,5 @@ def crawl(target_url, log) -> set:
             if url.startswith(target_url):
                 if url not in urls:
                     urls.add(url)
-
-    temp_urls = set()
-    # ^^^ to prevent "RuntimeError: Set changed size during iteration"
-
-    if len(urls) < 10:
-        for each_url in urls:
-            try:
-                reqs = get(each_url)
-                soup = BeautifulSoup(reqs.text, "html.parser")
-            except ConnectionError:
-                log.logger(
-                    "error",
-                    f"Connection error raised on: {each_url}, skipping ..."
-                )
-                continue
-            else:
-                for link in soup.find_all("a", href=True):
-                    url = link["href"]
-                    if url == "" or url is None or "#" in url:
-                        continue
-
-                    if not url.startswith("http"):
-                        if url.startswith("./"):
-                            url = f"{each_url}{url.lstrip('./')}"
-                        elif url.startswith("/"):
-                            url = f"{each_url}{url.lstrip('/')}"
-                        else:
-                            url = f"{each_url}{url}"
-
-                        if url not in urls:
-                            temp_urls.add(url)
-                    else:
-                        if url.startswith(each_url):
-                            if url not in urls:
-                                temp_urls.add(url)
-    for url in temp_urls:
-        urls.add(url)
 
     return urls
