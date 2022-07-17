6990d6a08e05ae143df68aa6a749ecc77dc529f2
GamehunterKaan <kaangultekin01@gmail.com>
2022-07-17 23:22:47 +0300

    Working on ...

--- api.py
+++ api.py
@@ -8,6 +8,36 @@ from modules.utils import is_root
 
 JSON = Union[Dict[str, Any], List[Any], int, str, float, bool, Type[None]]
 
+
+def InitHostInfo(target_key):
+    try:
+        mac = target_key["addresses"]["mac"]
+    except (KeyError, IndexError):
+        mac = "Unknown"
+
+    try:
+        vendor = target_key["vendor"][0]
+    except (KeyError, IndexError):
+        vendor = "Unknown"
+
+    try:
+        os = target_key["osmatch"][0]["name"]
+    except (KeyError, IndexError):
+        os = "Unknown"
+
+    try:
+        os_accuracy = target_key["osmatch"][0]["accuracy"]
+    except (KeyError, IndexError):
+        os_accuracy = "Unknown"
+
+    try:
+        os_type = target_key["osmatch"][0]["osclass"][0]["type"]
+    except (KeyError, IndexError):
+        os_type = "Unknown"
+
+    return mac, vendor, os, os_accuracy, os_type
+
+
 class AutoScanner:
     class fake_logger:
         def logger(self, exception_ : str, message : str):
@@ -18,7 +48,7 @@ class AutoScanner:
 
     def __str__(self) -> str:
         return str(self.scan_results)
- 
+
     def scan(
             self,
             target,
@@ -65,14 +95,15 @@ class AutoScanner:
             else:
                 self.scan_results[host] = {}
                 self.scan_results[host]["ports"] = port_scan
-                if "-O" in scan_args:
-                    os_info = {}
-                    os_info["mac"] = nm[host]["addresses"]["mac"]
-                    os_info["vendor"] = nm[host]["vendor"][0]
-                    os_info["os"] = nm[host]["osmatch"][0]["name"]
-                    os_info["accuracy"] = nm[host]["osmatch"][0]["accuracy"]
-                    os_info["type"] = nm[host]["osmatch"][0]["osclass"][0]["type"]
-                    self.scan_results[host]["os"] = os_info
+
+            if "-O" in scan_args:
+                os_info = {}
+                os_info["mac"] = nm[host]["addresses"]["mac"]
+                os_info["vendor"] = nm[host]["vendor"][0]
+                os_info["os"] = nm[host]["osmatch"][0]["name"]
+                os_info["accuracy"] = nm[host]["osmatch"][0]["accuracy"]
+                os_info["type"] = nm[host]["osmatch"][0]["osclass"][0]["type"]
+                self.scan_results[host]["os"] = os_info
 
             vulns = {}
             for port in nm[host]["tcp"]:
@@ -81,30 +112,35 @@ class AutoScanner:
                 version = nm[host]["tcp"][port]["version"]
 
                 keyword = f"{product} {version}"
+
+                if debug:
+                    print(f"Searching for keyword {keyword}")
+
                 Vulnerablities = searchCVE(keyword, log, apiKey)
-                if len(Vulnerablities) > 0:
-                    vulns["product"] = {}
-                    for vuln in Vulnerablities:
-                        vulns["product"][vuln.CVEID] = {}
-                        vulns["product"][vuln.CVEID]["description"] = vuln.description
-                        vulns["product"][vuln.CVEID]["severity"] = vuln.severity
-                        vulns["product"][vuln.CVEID]["severity_score"] = vuln.severity_score
-                        vulns["product"][vuln.CVEID]["details_url"] = vuln.details_url
-                        vulns["product"][vuln.CVEID]["exploitability"] = vuln.exploitability
+                
+                if len(Vulnerablities) == 0:
+                    continue
+
+                vulns["product"] = {}
+                for vuln in Vulnerablities:
+                    vulns["product"][vuln.CVEID] = {}
+                    vulns["product"][vuln.CVEID]["description"] = vuln.description
+                    vulns["product"][vuln.CVEID]["severity"] = vuln.severity
+                    vulns["product"][vuln.CVEID]["severity_score"] = vuln.severity_score
+                    vulns["product"][vuln.CVEID]["details_url"] = vuln.details_url
+                    vulns["product"][vuln.CVEID]["exploitability"] = vuln.exploitability
 
                 self.scan_results[host]["vulns"] = vulns
-
 
         if output_file:
             with open(output_file, "w") as output:
                 json_object = dumps(self.scan_results)
                 output.write(json_object)
 
-        print(self.scan_results)
-        print(self.scan_results["192.168.0.29"].keys())
         return self.scan_results
 
 scanner = AutoScanner()
 results = scanner.scan("192.168.0.29")
 print(results)
-print(results.keys())
+print(results["192.168.0.29"].keys())
+print(results["192.168.0.29"]["vulns"].keys())
